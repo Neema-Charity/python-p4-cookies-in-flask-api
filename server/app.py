77082c2797738aa4bb61ae8d397bf8088d606aca
Neema-Charity <ncharityyy@gmail.com>
2024-07-05 10:53:21 +0300

    code along

--- server/app.py
+++ server/app.py
@@ -8,8 +8,8 @@ app.secret_key = b'?w\x85Z\x08Q\xbdO\xb8\xa9\xb65Kj\xa9_'
 @app.route('/sessions/<string:key>', methods=['GET'])
 def show_session(key):
 
-    session["hello"] = session.get("hello") or "World"
-    session["goodnight"] = session.get("goodnight") or "Moon"
+    session["hello"] = session.get("hello") or "World" # Sets to World if called the first time
+    session["goodnight"] = session.get("goodnight") or "Moon" # Sets toMoon if called the first time
 
     response = make_response(jsonify({
         'session': {
@@ -17,11 +17,11 @@ def show_session(key):
             'session_value': session[key],
             'session_accessed': session.accessed,
         },
-        'cookies': [{cookie: request.cookies[cookie]}
+        'cookies': [{cookie: request.cookies[cookie]} #Key-value pair(dictionary)
             for cookie in request.cookies],
     }), 200)
 
-    response.set_cookie('mouse', 'Cookie')
+    response.set_cookie('mouse', 'Cookie') # Key of mouse and value pf Cookie
 
     return response
 
